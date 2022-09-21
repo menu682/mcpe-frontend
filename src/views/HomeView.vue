f9d36d245e5a993bcf047d0851006a503fde0073
Fenix <menu682@gmail.com>
2022-09-21 16:34:56 +0300

    login form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+        <div class="d-flex justify-content-center align-items-center fs-1 fw-bold">
+            <h1 class="text-danger">ACCESS DENIED!!!</h1>
+        </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center">
+        <form class="d-flex flex-column">
+            <div class="mb-3 d-flex flex-column justify-content-center align-items-center">
+                <label for="login" class="form-label">Login</label>
+                <input type="text" class="form-control" id="login">
+            </div>
+            <div class="mb-3 d-flex flex-column justify-content-center align-items-center">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary d-flex justify-content-center">GO</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginView",
+    data(){
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
